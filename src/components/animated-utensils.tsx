ff06bfec3d9628fa1bcbac3967debd310d443c45
Ponsangkar Murugesan <sangkar111@gmail.com>
2025-07-08 10:00:51 +0000

    knife is missing a handle

--- src/components/animated-utensils.tsx
+++ src/components/animated-utensils.tsx
@@ -1,4 +1,3 @@
-
 "use client";
 
 import { cn } from "@/lib/utils";
@@ -24,6 +23,7 @@ export function AnimatedUtensils({ className }: { className?: string }) {
       </g>
       {/* Knife */}
       <g className="origin-center transition-transform duration-300 ease-in-out group-hover:rotate-[15deg] group-hover:translate-x-[3px] group-hover:translate-y-[1px]">
+        <path d="M16 2v20" />
         <path d="M21 15V2v0a5 5 0 0 0-5 5v6c0 1.1.9 2 2 2h3z" />
       </g>
     </svg>
